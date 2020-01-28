10392950c98f841b75df217a795986fdcb83aa54
Dzianis Kotau <jampire.blr@gmail.com>
2020-01-29 02:24:23 +0300

    Video #37

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, I'm here</h1>
+      <Person name="Den" age="35" />
+      <Person name="Arsen" age="24">Hobbies: Laziness</Person>
+      <Person name="Denis" age="29" />
     </div>
   );
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function person(props) {
+    return (
+        <div>
+            <p>I'm {props.name} and I'm {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
+
+    );
+}
+
+export default person;
